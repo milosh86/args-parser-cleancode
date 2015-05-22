f9c05d007062ec8ec85bbcb2522444e2df2a197a
Milosh <milos.dzepina@pstech.rs>
2015-05-22 17:44:08 +0200

    added enum

---
+++ argsExceptionEnum.js
@@ -0,0 +1,17 @@
+export default
+let argsExceptionEnum = {
+    INVALID_ARGUMENT_FORMAT: 'Invalid argument format',
+    INVALID_ARGUMENT_NAME: 'Invalid argument name',
+    UNEXPECTED_ARGUMENT: 'Unexpected command-line argument',
+    MISSING_STRING: 'Missing String',
+    MISSING_OR_INVALID_NUMBER: 'Missing or invalid Number'
+};
+
+
+//class ArgsException {
+//    constructor(message, data) {
+//        this.message = message;
+//        this.data = data;
+//        this.name = "ArgsException";
+//    }
+//}
